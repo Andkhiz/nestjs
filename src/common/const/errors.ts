9b7ea6e16d373477895f649249304d22968c3fa8
andkhiz <andkhiz@gmail.com>
2024-06-23 21:08:35 +0300

    feat: add const

---
+++ src/common/const/errors.ts
@@ -0,0 +1,5 @@
+export const AppError = {
+  USER_EXIST: 'User with this email already exist',
+  USER_NOT_EXIST: 'User with this email not exist',
+  WRONG_USER_DATA: 'Wrong username or password',
+};
