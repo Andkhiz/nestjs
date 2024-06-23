cc5fcdc126e7eaba5f795ec017bb10280342d2c9
andkhiz <andkhiz@gmail.com>
2024-06-23 15:01:27 +0300

    init repository

---
+++ src/moks/index.ts
@@ -0,0 +1,227 @@
+export const users = `[
+    {
+      "_id": "66767329784c86f99b771e61",
+      "index": 0,
+      "guid": "49d6bbe0-5ab3-42e3-93e3-5d56c928a3fb",
+      "isActive": true,
+      "balance": "$1,590.06",
+      "picture": "http://placehold.it/32x32",
+      "age": 26,
+      "eyeColor": "blue",
+      "name": "Hancock Gonzalez",
+      "gender": "male",
+      "company": "NIPAZ",
+      "email": "hancockgonzalez@nipaz.com",
+      "phone": "+1 (990) 453-2530",
+      "address": "167 Arlington Place, Gadsden, Montana, 2272",
+      "about": "Duis enim cillum incididunt proident tempor. Velit sunt labore officia sint reprehenderit aute elit aute dolor. Ea voluptate tempor do ipsum ipsum exercitation duis et aliquip ipsum cupidatat exercitation. Occaecat amet est id enim cupidatat. Nostrud nulla id in nisi magna incididunt proident occaecat velit veniam labore voluptate.\r\n",
+      "registered": "2018-11-03T05:56:25 -02:00",
+      "latitude": -69.241279,
+      "longitude": 152.526993,
+      "tags": [
+        "Lorem",
+        "esse",
+        "proident",
+        "esse",
+        "sunt",
+        "aute",
+        "deserunt"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Jenny Dodson"
+        },
+        {
+          "id": 1,
+          "name": "Lancaster Watson"
+        },
+        {
+          "id": 2,
+          "name": "Deleon Middleton"
+        }
+      ],
+      "greeting": "Hello, Hancock Gonzalez! You have 5 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "66767329f6f91ffcf885616c",
+      "index": 1,
+      "guid": "1dee15a4-0c4d-445f-ba97-35486b325a6c",
+      "isActive": false,
+      "balance": "$3,906.45",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "brown",
+      "name": "Carson Riggs",
+      "gender": "male",
+      "company": "NETUR",
+      "email": "carsonriggs@netur.com",
+      "phone": "+1 (989) 401-2211",
+      "address": "629 Desmond Court, Dellview, Idaho, 8006",
+      "about": "Ex minim cupidatat occaecat ullamco duis eiusmod non dolor magna fugiat. Cillum labore excepteur et occaecat fugiat magna ipsum reprehenderit esse minim cupidatat. Exercitation minim ipsum ex quis nisi adipisicing velit quis excepteur minim. Esse est duis proident velit fugiat veniam. Aute occaecat pariatur commodo culpa aliqua Lorem ex nulla veniam sint. Nostrud aute eu sint quis sint ullamco tempor nisi occaecat Lorem irure.\r\n",
+      "registered": "2021-07-22T10:09:42 -03:00",
+      "latitude": 55.685448,
+      "longitude": 101.872176,
+      "tags": [
+        "dolor",
+        "elit",
+        "laboris",
+        "reprehenderit",
+        "tempor",
+        "mollit",
+        "aliqua"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Atkinson Murray"
+        },
+        {
+          "id": 1,
+          "name": "Dickson Woodward"
+        },
+        {
+          "id": 2,
+          "name": "Jacklyn Porter"
+        }
+      ],
+      "greeting": "Hello, Carson Riggs! You have 1 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "66767329b7f41c9b73d967ef",
+      "index": 2,
+      "guid": "ef003d0d-c60f-4065-b895-8f858d13feda",
+      "isActive": false,
+      "balance": "$2,129.91",
+      "picture": "http://placehold.it/32x32",
+      "age": 39,
+      "eyeColor": "green",
+      "name": "Fischer Holcomb",
+      "gender": "male",
+      "company": "PROSELY",
+      "email": "fischerholcomb@prosely.com",
+      "phone": "+1 (957) 530-3731",
+      "address": "562 Seba Avenue, Enlow, Virginia, 8119",
+      "about": "Id dolore irure velit irure aliquip. Occaecat reprehenderit ullamco ut ut cupidatat esse irure in id consectetur. Consequat commodo ad sunt aliquip ad velit dolore occaecat minim. Irure consequat deserunt ullamco labore dolore deserunt in laboris. Dolore consectetur cupidatat eiusmod cupidatat amet.\r\n",
+      "registered": "2023-12-06T02:09:09 -02:00",
+      "latitude": 65.544568,
+      "longitude": -118.818641,
+      "tags": [
+        "reprehenderit",
+        "eu",
+        "ad",
+        "eu",
+        "nostrud",
+        "duis",
+        "ea"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Brandi Aguilar"
+        },
+        {
+          "id": 1,
+          "name": "Stephens Duffy"
+        },
+        {
+          "id": 2,
+          "name": "Sharlene Logan"
+        }
+      ],
+      "greeting": "Hello, Fischer Holcomb! You have 4 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "667673290d831f6e21db3eb1",
+      "index": 3,
+      "guid": "444f7444-8972-4cee-a96b-997dbfccd8fc",
+      "isActive": false,
+      "balance": "$1,491.44",
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Beverly Bridges",
+      "gender": "female",
+      "company": "GUSHKOOL",
+      "email": "beverlybridges@gushkool.com",
+      "phone": "+1 (966) 479-3917",
+      "address": "799 Revere Place, Cornucopia, Rhode Island, 414",
+      "about": "Ad ea officia commodo mollit. Duis nulla ullamco deserunt officia qui. Velit tempor fugiat voluptate minim incididunt eu cupidatat qui anim minim ad labore. Nulla dolor deserunt tempor anim. Sint nisi enim laboris sunt. Esse incididunt aliquip eu mollit sit.\r\n",
+      "registered": "2015-02-27T12:45:36 -02:00",
+      "latitude": -67.325661,
+      "longitude": -108.037575,
+      "tags": [
+        "incididunt",
+        "culpa",
+        "veniam",
+        "non",
+        "nulla",
+        "voluptate",
+        "fugiat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Oliver Vasquez"
+        },
+        {
+          "id": 1,
+          "name": "Earlene Mayer"
+        },
+        {
+          "id": 2,
+          "name": "Morgan Suarez"
+        }
+      ],
+      "greeting": "Hello, Beverly Bridges! You have 8 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "66767329acd08458ee1ace32",
+      "index": 4,
+      "guid": "d396e2f0-554b-43d7-9fdf-4dd95296f792",
+      "isActive": false,
+      "balance": "$2,526.04",
+      "picture": "http://placehold.it/32x32",
+      "age": 27,
+      "eyeColor": "blue",
+      "name": "Maddox Patrick",
+      "gender": "male",
+      "company": "AUTOGRATE",
+      "email": "maddoxpatrick@autograte.com",
+      "phone": "+1 (937) 580-2396",
+      "address": "489 Schenck Place, Bluetown, Guam, 4643",
+      "about": "Cillum ex ut id eiusmod irure proident nulla elit anim. Minim Lorem dolor do magna fugiat proident aliquip do proident. Occaecat eu aliqua in aute commodo dolore esse mollit amet. Deserunt exercitation sit ea non tempor laborum id velit sint enim. Officia excepteur magna occaecat nulla laboris cillum. Occaecat adipisicing in dolore dolore.\r\n",
+      "registered": "2022-05-06T06:46:18 -03:00",
+      "latitude": 33.288534,
+      "longitude": 163.963204,
+      "tags": [
+        "proident",
+        "do",
+        "cupidatat",
+        "eu",
+        "amet",
+        "amet",
+        "ipsum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Austin Rosa"
+        },
+        {
+          "id": 1,
+          "name": "Ann Mejia"
+        },
+        {
+          "id": 2,
+          "name": "Boyd Dillon"
+        }
+      ],
+      "greeting": "Hello, Maddox Patrick! You have 8 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]`;
